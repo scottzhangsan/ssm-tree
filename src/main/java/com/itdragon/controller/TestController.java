cacb6691ff5e783192eeeb799af6bed79b490e7f
zhangssan <1154769609@qq.com>
2018-01-04 15:14:02 +0800

    项目框架搭建初始化

---
+++ src/main/java/com/itdragon/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.itdragon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String test(){
+		
+		return "hello" ;
+	}
+
+}
