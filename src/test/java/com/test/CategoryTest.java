be7eb84aa21246a324034c22449f11af33d484df
zhangssan <1154769609@qq.com>
2018-01-05 14:17:43 +0800

    添加单元测试

---
+++ src/test/java/com/test/CategoryTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.util.List;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;  
+import com.itdragon.common.pojo.EUTreeNode;
+import com.itdragon.service.CategoryService;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:spring/applicationContext-*.xml","classpath:spring/springmvc.xml"})
+public class CategoryTest {
+	@Autowired
+	private CategoryService categoryService ;
+	@Test
+	public void testCategory(){
+		List<EUTreeNode> categories =categoryService.getCategoryList();
+		
+		Assert.assertNotEquals(categories.size(), 0);
+		Assert.assertNotNull(categories);
+		
+	}
+ 
+}
