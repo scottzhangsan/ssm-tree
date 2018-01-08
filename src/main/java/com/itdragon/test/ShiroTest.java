59e1dbf06a10c7ea0a781bf26c3443f609b4e397
zhangyazhou <yazhou.zhang@hpe.com>
2018-01-08 13:35:22 +0800

    添加shiro realm

--- src/main/java/com/itdragon/test/ShiroTest.java
+++ src/main/java/com/itdragon/test/ShiroTest.java
@@ -15,7 +15,7 @@ public class ShiroTest {
 	@Test
 	public void testHelloWold(){
 		//1:获取SecurityManager的工厂。
-		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini") ;
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini") ;
 		//2:获取securityManage的实例并绑定给SecurityUtil
 		SecurityManager securityManager =factory.getInstance() ;
 		
@@ -23,7 +23,7 @@ public class ShiroTest {
 		//3:获取subject的实例。创建用户名，密码进行身份验证。
 	    Subject subject = SecurityUtils.getSubject() ;
 	    
-	    UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("zhang1", "123") ;
+	    UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("zhang", "123") ;
 	    
 	    try {//4:登录身份验证
 	    	subject.login(usernamePasswordToken);
